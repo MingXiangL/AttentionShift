f655a04ad64281e3eef48d4e6229bf06dae161f4
liaomx <49087948+MingXiangL@users.noreply.github.com>
2023-08-21 15:58:30 +0800

    Update run_train.py
    Signed-off-by: liaomx <49087948+MingXiangL@users.noreply.github.com>

--- run_train.py
+++ run_train.py
@@ -12,3 +12,10 @@ if __name__ == "__main__":
                 data.workers_per_gpu=2 \
                 optimizer_config.update_interval=2 \
               --gpus 8 --launcher pytorch")
+    # aug test:
+    os.system(f"python -m torch.distributed.launch --nproc_per_node=8 --master_port=50041 --use_env ./tools/test.py \
+            {config} \
+            work_dir/epoch_12.pth \
+            --out work_dir/bbox_predict_1.pkl \
+            --eval mAP_Segm \
+            --launcher pytorch")
